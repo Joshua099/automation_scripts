2ec18c77061f09541d3a73059c98b70d675638b8
Joshua Pantoja <38227841+Joshua099@users.noreply.github.com>
2019-05-03 13:20:52 -0700

    Update consolidate.py

--- consolidate.py
+++ consolidate.py
@@ -3,25 +3,25 @@ import csv
 import os
 
 # Created an iterable list of csvs
-folder = [os.path.relpath(x) for x in os.listdir('C:/Users/joshua/PycharmProjects/xlsx_consolidation/AD Users/')]
+folder = [os.path.relpath(x) for x in os.listdir('dir')]
 
 # Created a string object containing the folder location to dump the files into
-root = 'C:/Users/joshua/PycharmProjects/xlsx_consolidation/dump'
+root = 'dir'
 
 # Loops through each file, adds a new column with values containing orig. filename
 for file in folder:
-    df = pd.read_csv('C:/Users/joshua/PycharmProjects/xlsx_consolidation/AD Users/'+file, header=0, index_col=0)
+    df = pd.read_csv('dir'+file, header=0, index_col=0)
     o = os.path.basename(file)
     df['Original Filename'] = o
     df.to_csv(root + '/' + file) # saves to designated root folder
 
 
 # created an iterable list of the csv's with the original filename column
-root2 = [os.path.relpath(x) for x in os.listdir('C:/Users/joshua/PycharmProjects/xlsx_consolidation/dump/')]
+root2 = [os.path.relpath(x) for x in os.listdir('dir')]
 
 
 # Combines all csv's into a dataframe
-combined_csv = pd.concat([pd.read_csv('C:/Users/joshua/PycharmProjects/xlsx_consolidation/dump/'+f) for f in root2], sort=False)
+combined_csv = pd.concat([pd.read_csv('dir'+f) for f in root2], sort=False)
 
 # loads combined csvs into a file
 combined_csv.to_csv("combined_csv.csv", index=0)
